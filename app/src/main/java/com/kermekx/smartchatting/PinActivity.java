4c44773f98b98150dd0acdcd4aa068b914f8ffd4
kermekx <kermekx@gmail.com>
2016-03-01 14:09:54 +0100

    add emoji keyboard in conversation

---
+++ app/src/main/java/com/kermekx/smartchatting/PinActivity.java
@@ -0,0 +1,13 @@
+package com.kermekx.smartchatting;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PinActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin);
+    }
+}
