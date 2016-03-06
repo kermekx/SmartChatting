54ea77a360b42e755a976192671eabf78b33ef6f
kermekx <kermekx@gmail.com>
2016-03-06 14:58:58 +0100

    Unlock 100% done

--- app/src/main/java/com/kermekx/smartchatting/PinActivity.java
+++ app/src/main/java/com/kermekx/smartchatting/PinActivity.java
@@ -1,11 +1,21 @@
 package com.kermekx.smartchatting;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.kermekx.smartchatting.hash.Hasher;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PinActivity extends AppCompatActivity {
 
@@ -17,6 +27,25 @@ public class PinActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pin);
 
         mPinView = (EditText) findViewById(R.id.pin);
+        mPinView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView view, int id, KeyEvent event) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    unlock(mPinView.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.unlock_button);
+        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unlock(mPinView.getText().toString());
+            }
+        });
+
 
         SharedPreferences settings = getSharedPreferences(getString(R.string.preference_file_session), 0);
 
@@ -27,5 +56,24 @@ public class PinActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    private void unlock(String pin) {
+        SharedPreferences settings = getSharedPreferences(getString(R.string.preference_file_session), 0);
+        String pinBackup = settings.getString("pinCheck", "");
+
+        mPinView.setError(null);
+
+        if (Hasher.md5(pin).equals(pinBackup)) {
+            Bundle extras = new Bundle();
+            extras.putByteArray("pin", Hasher.sha256Byte(pin));
+
+            Intent mainActivity = new Intent(PinActivity.this, MainActivity.class);
+            mainActivity.putExtras(extras);
+            PinActivity.this.startActivity(mainActivity);
+        } else {
+            mPinView.setError(getString(R.string.error_incorrect_pin));
+            mPinView.requestFocus();
+        }
     }
 }
